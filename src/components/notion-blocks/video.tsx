489e33c50115b2e9e9c47b0d14ec50b222987667
herohoro <mtwd.office.25@gmail.com>
2022-06-18 16:29:54 +0900

    youtube props_ライブラリExample参考

--- src/components/notion-blocks/video.tsx
+++ src/components/notion-blocks/video.tsx
@@ -1,29 +1,39 @@
-import YouTube from 'react-youtube';
-
 import styles from '../../styles/notion-block.module.css'
 
-const Video=({url}) => {
+import React from 'react';
+import YouTube, { YouTubeProps } from 'react-youtube';
+
+const Video=({url})=> {
     let matched
     try {
-        matched = new URL(url).pathname.match(/\/(\w+)$/)
+        matched = new URL(url).pathname.match(/\/[a-zA-Z0-9-_]{11}$/)
     } catch (error) {
         console.log(error)
-        return (
-            <>
-            <p>マッチできていません</p>
-            </>
-        )
+        return null
     }
 
     if (!matched) {
         return null
     }
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
 
   return (
-   <div className={styles.Video}>
-    <YouTube videoId={matched[1]} />
-   </div> 
-  );
+    <div className={styles.Video}>
+        <YouTube videoId={matched[1]} opts={opts} onReady={onPlayerReady} />
+    </div> 
+  )
 }
 
 export default Video;
