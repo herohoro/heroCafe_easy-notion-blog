1fac7fd980fe32f24a7c4d2496b926ad8e08466a
herohoro <mtwd.office.25@gmail.com>
2022-06-19 20:45:15 +0900

    🎉 Support youtube-video_embed

--- src/components/notion-blocks/video.tsx
+++ src/components/notion-blocks/video.tsx
@@ -2,10 +2,12 @@ import styles from '../../styles/notion-block.module.css'
 import React from 'react';
 import YouTube, {YouTubeProps } from 'react-youtube';
 
+
 const Video=({block})=> {
   const url = block.Video.External.Url
   const VIDEOS =url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
-  // const videoID=""+VIDEOS[1]+"";
+  const videoID=""+VIDEOS[1]+"";
+
   //npm
   const onPlayerReady: YouTubeProps['onReady'] = (event) => {
     event.target.pauseVideo();
@@ -17,16 +19,14 @@ const Video=({block})=> {
       autoplay: 1,
     },
   };
-  // console.log(VIDEOS)
-  
+  console.log(VIDEOS)
+
   return (
     <div className={styles.Video}>
-      {/* <p>
-        取得videoID______   {videoID}
-      </p> */}
-      <YouTube videoId={VIDEOS[1]} opts={opts} onReady={onPlayerReady} className={styles.youtube}/>
+      <YouTube videoId={videoID} opts={opts} onReady={onPlayerReady} className={styles.youtube}/>
     </div>
   );
 }
 
-export default Video;
+
+export default Video; 
