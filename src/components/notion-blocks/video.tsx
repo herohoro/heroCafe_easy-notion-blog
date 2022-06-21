69113e72f9624ea54e2b7212fadbf3f7b3fb32ab
herohoro <mtwd.office.25@gmail.com>
2022-06-21 19:24:28 +0900

    不要な記述精査_video-embed

--- src/components/notion-blocks/video.tsx
+++ src/components/notion-blocks/video.tsx
@@ -5,10 +5,9 @@ import YouTube, {YouTubeProps } from 'react-youtube';
 const Video=({block})=> {
   const url = block.Video.External.Url
   const VIDEOS =url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
-  const videoID=""+VIDEOS[1]+"";
+  // const videoID=""+VIDEOS[1]+"";
   //npm
   const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-    // access to player in all event handlers via event.target
     event.target.pauseVideo();
   }
   const opts: YouTubeProps['opts'] = {
@@ -18,14 +17,14 @@ const Video=({block})=> {
       autoplay: 1,
     },
   };
-  console.log(VIDEOS)
+  // console.log(VIDEOS)
   
   return (
     <div className={styles.Video}>
       {/* <p>
         取得videoID______   {videoID}
       </p> */}
-      <YouTube videoId={videoID} opts={opts} onReady={onPlayerReady} className={styles.youtube}/>
+      <YouTube videoId={VIDEOS[1]} opts={opts} onReady={onPlayerReady} className={styles.youtube}/>
     </div>
   );
 }
