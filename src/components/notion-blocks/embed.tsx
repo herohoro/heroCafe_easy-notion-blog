deec0ac9faf321a5d8a013a2400c78e33eacb73a
herohoro <mtwd.office.25@gmail.com>
2022-06-22 14:55:11 +0900

    fix twitter-embed

--- src/components/notion-blocks/embed.tsx
+++ src/components/notion-blocks/embed.tsx
@@ -3,7 +3,6 @@ import dynamic from 'next/dynamic'
 import styles from '../../styles/notion-block.module.css'
 
 const TweetEmbed = dynamic(() => import('./tweet-embed'))
-// const Video = dynamic(() => import('./video'))
 const LinkPreview = dynamic(() =>
   import('@dhaiwat10/react-link-preview').then(m => m.LinkPreview)
 )
@@ -11,10 +10,6 @@ const LinkPreview = dynamic(() =>
 const Embed = ({ block }) => {
   if (/^https:\/\/twitter\.com/.test(block.Embed.Url)) {
     return <TweetEmbed url={block.Embed.Url} />
-  // } else if (/^https:\/\/www\.youtube\.com/.test(block.Embed.Url)) {
-  //   return <Video url={block.Embed.Url} />
-  // } else if (/^https:\/\/youtu\.be\.com/.test(block.Embed.Url)){
-  //   return <Video url={block.Embed.Url} />
   } else if (/^https:\/\/gist\.github\.com/.test(block.Embed.Url)) {
     return <LinkPreview url={block.Embed.Url} className={styles.linkPreview} />
   }
