45dd1f27e445bb00a851209c55c0b87fcb14252e
herohoro <mtwd.office.25@gmail.com>
2022-07-10 17:11:31 +0900

    Fix 深夜学習status

--- src/pages/herospace/index.tsx
+++ src/pages/herospace/index.tsx
@@ -47,7 +47,11 @@ const RenderPostsSpace = ({
         {secPosts.map(secPost => {
           return(
             <div className={SecStyles.card} key={secPost.title}>
-            
+            <div >
+              <div className={`${secPost.siteCollor}`}>
+                <p>{secPost.site}</p>
+              </div>
+            </div>
             <h3>{secPost.date} ~</h3>
             <Link href={secPost.URL} passHref><p>📝 {secPost.title}</p></Link>
             <p>&#128537; {(secPost.description)?(secPost.description):null}</p>
