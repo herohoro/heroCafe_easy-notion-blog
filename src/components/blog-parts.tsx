ec8618ece4d732f1fcfe3916903f354ef2d2afa4
herohoro <mtwd.office.25@gmail.com>
2022-07-11 03:14:49 +0900

    vercel-error

--- src/components/blog-parts.tsx
+++ src/components/blog-parts.tsx
@@ -12,7 +12,6 @@ import {
   getTagBeforeLink,
 } from '../lib/blog-helpers'
 import styles from '../styles/blog-parts.module.css'
-import ContentIndex from './content-index'
 
 export const PostDate = ({ post }) => (
   <div className={styles.postDate}>
