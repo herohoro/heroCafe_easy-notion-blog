f5b26c7d50619dc0284cc6b301121954692441e5
herohoro <mtwd.office.25@gmail.com>
2022-07-11 04:00:53 +0900

    fix responsive

--- src/pages/herospace/index.tsx
+++ src/pages/herospace/index.tsx
@@ -50,8 +50,8 @@ const RenderPostsSpace = ({
             <div className={SecStyles.fukidashi}>
               <Image
                 src="/fukidashi.png"
-                width={50}
-                height={50}
+                width={100}
+                height={100}
                 objectFit="contain"
                 alt=""
               />
