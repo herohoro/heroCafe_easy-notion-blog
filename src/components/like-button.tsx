ab181022d3e411a480df89e030c77ceb578d0d22
herohoro <mtwd.office.25@gmail.com>
2022-07-17 21:58:21 +0900

    Update button-count_num

--- src/components/like-button.tsx
+++ src/components/like-button.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import axios from 'axios'
 
 import styles from '../styles/like-button.module.css'
@@ -10,20 +10,22 @@ type Props = {
 }
 
 const LikeButton = (props: Props) => {
+  const [count, setCount] = useState(props.like)
   const [active, setActive] = useState(false)
 
   const handleClick = () => {
     if (!active) {
       axios.put(`/api/like?slug=${props.id}`, {})
       setActive(true)
+      setCount(count + 1)
     }
   }
 
   return (
     <button className={styles.likeButton} onClick={handleClick}>
-        <p>へろちゃんへsubmit 🚀</p>
+      <p>へろちゃんへsubmit 🚀</p>
       <Heart width={32} height={32} active={active} />
-      <div className={styles.likeCount} > {props.like}</div>
+      <div className={styles.likeCount}>{count} </div>
     </button>
   )
 }
