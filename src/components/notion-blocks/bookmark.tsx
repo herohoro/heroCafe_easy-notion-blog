5449f86a3a639370537f7a3d80c849c6766953d9
otoyo <hiroki.t.1988@gmail.com>
2022-08-16 22:16:07 +0900

    Reform Bookmark component

--- src/components/notion-blocks/bookmark.tsx
+++ src/components/notion-blocks/bookmark.tsx
@@ -1,50 +1,76 @@
-import { LinkPreview } from '@dhaiwat10/react-link-preview'
-import { NEXT_PUBLIC_URL } from '../../lib/notion/server-constants'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import styles from '../../styles/notion-block.module.css'
-import Link from 'next/link'
+
+interface Metadata {
+  title: string | null
+  description: string | null
+  image: string | null
+}
 
 const Bookmark = ({ block }) => {
-  const url = block.Bookmark ? block.Bookmark.Url : block.LinkPreview.Url
-  // {new URL('/error.jpg', NEXT_PUBLIC_URL).toString()}
-  const result = url.indexOf(NEXT_PUBLIC_URL.toString())
-  
-    if(result !== -1){
-      // 見つかった時
-      return (
-        <div className={styles.MylinkPreview}>
-          <Link href={url.toString()} passHref >
-            <p>へろほろブログの記事はこちら</p>
-          </Link>
-        </div>
-      // url={url}
-      // className={styles.MylinkPreview}
-      // fallbackImageSrc={new URL('/error.png', NEXT_PUBLIC_URL).toString()}
-      // backgroundColor="#d5d1ed"
-      // fallbackImageSrc="https://herohoro.com/default.png"
-      // imageHeight='120wh'
-      
-      // borderColor='#8c8676'
-      // descriptionLength={60}
-      
-    
-      )
+  let sURL: string | null
+  if (block.Bookmark) {
+    sURL = block.Bookmark.Url
+  } else if (block.LinkPreview) {
+    sURL = block.LinkPreview.Url
+  } else if (block.Embed) {
+    sURL = block.Embed.Url
+  }
+
+  const [metadata, setMetadata] = useState<Metadata | null>()
+
+  useEffect(() => {
+    try {
+      const url = new URL(sURL)
+      axios.get(`/api/url-metadata?url=${url.toString()}`).then((res) => {
+        setMetadata(res.data as Metadata)
+      })
+    } catch (e) {
+      console.log(e)
     }
-    else{
-      return(
-      <LinkPreview
-      url={url}
-      className={styles.linkPreview}
-      fallbackImageSrc={new URL('/error.png', NEXT_PUBLIC_URL).toString()}
-      backgroundColor="#f7ebe1"
-      // fallbackImageSrc="https://herohoro.com/default.png"
-      imageHeight='100wh'
-      
-      borderColor='#8c8676'
-      descriptionLength={60}
-      
-    /> 
+  }, [sURL])
+
+  let url: URL
+  try {
+    url = new URL(sURL)
+  } catch (e) {
+    console.log(e)
+  }
+
+  if (!metadata || !url) {
+    return <></>
+  }
+
+  const { title, description, image } = metadata
+
+  return (
+    <div className={styles.bookmark}>
+      <a href={url.toString()} target="_blank" rel="noopener noreferrer">
+        <div>
+          <div>{title ? title : ''}</div>
+          <div>{description ? description : ''}</div>
+          <div>
+            <div>
+              <img
+                src={`http://www.google.com/s2/favicons?domain=${url.hostname}`}
+                alt="title"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <div>{url.origin}</div>
+          </div>
+        </div>
+        <div>
+          {image ? (
+            <img src={image} alt="title" loading="lazy" decoding="async" />
+          ) : null}
+        </div>
+      </a>
+    </div>
   )
- }
 }
 
 export default Bookmark
