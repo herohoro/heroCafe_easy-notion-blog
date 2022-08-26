53737a10a20e541bd859e6ae6f9e17275e451676
otoyo <hiroki.t.1988@gmail.com>
2022-08-26 13:01:26 +0900

    Refactor getting table rows

--- src/lib/notion/interfaces.ts
+++ src/lib/notion/interfaces.ts
@@ -35,7 +35,6 @@ export interface Block {
   Bookmark?: Bookmark
   LinkPreview?: LinkPreview
   Table?: Table
-  TableRow?: TableRow
 }
 
 export interface Paragraph {
@@ -130,10 +129,13 @@ export interface Table {
   TableWidth: number
   HasColumnHeader: boolean
   HasRowHeader: boolean
-  Rows: Block[]
+  Rows: TableRow[]
 }
 
 export interface TableRow {
+  Id: string
+  Type: string
+  HasChildren: boolean
   Cells: TableCell[]
 }
 
