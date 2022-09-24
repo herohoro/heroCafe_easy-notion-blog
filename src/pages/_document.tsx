6d57a5d9ad5829519e8e7fa539a754fb2346a2b2
herohoro <mtwd.office.25@gmail.com>
2022-09-25 00:01:15 +0900

    ちょっとgoogle翻訳の確認

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => (
-  <Html lang="ja">
+  <Html lang="en">
     <Head>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&family=Yomogi&family=Zen+Maru+Gothic&display=swap"
-      rel="stylesheet"
-    />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&family=Yomogi&family=Zen+Maru+Gothic&display=swap"
+        rel="stylesheet"
+      />
     </Head>
     <body>
       <Main />
