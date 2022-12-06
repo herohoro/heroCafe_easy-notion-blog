ade8c2e01266063fc682d439f801c94731bddf2e
herohoro <mtwd.office.25@gmail.com>
2022-12-06 14:21:40 +0900

    _Define RootPage

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,123 @@ import Link from 'next/link'
 import { NEXT_PUBLIC_SITE_TITLE } from './server-constants'
 import GoogleAnalytics from '../components/google-analytics'
 import styles from '../styles/page.module.css'
+import {
+  getPosts,
+  getRankedPosts,
+  getAllTags,
+  getAllBlocksByBlockId,
+  getAllCategorys,
+} from '../lib/notion/client'
+import { INDEX_PAGE_ID } from '../app/server-constants'
+import { getTagLink } from '../lib/blog-helpers'
+import Image from 'next/image'
+import NotionBlocks from '../components/notion-block'
+import SecStyles from '../styles/sec-notion.module.css'
+import {
+  BlogPostLink,
+  BlogTagLink,
+  TwitterTimeline,
+  BlogCategoryLink,
+} from '../components/blog-parts'
 
-const RootPage = () => (
-  <>
-    <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
-    <div className={styles.container}>
-      <div>
-        <h2>Welcome!</h2>
-        <p>Your easy-notion-blog deployed successfully!</p>
-        <p>Have fun!</p>
-        <p>
-          easy-notion-blog powered by{' '}
-          <Link href="https://github.com/otoyo/easy-notion-blog">
-            otoyo/easy-notion-blog
+export const revalidate = 60
+
+const RootPage = async () => {
+  const [blocks, posts, rankedPosts, tags, categorys] = await Promise.all([
+    getAllBlocksByBlockId(INDEX_PAGE_ID),
+    getPosts(),
+    getRankedPosts(),
+    getAllTags(),
+    getAllCategorys(),
+  ])
+  return (
+    <>
+      <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <div className={styles.flexTagsMain}>
+            {tags.map((tag) => {
+              if (tag === 'README' || tag === 'Q&A') {
+                return (
+                  <div className={styles.tagMain}>
+                    <Link href={getTagLink(tag)}>
+                      <p>easy-notion-blog&apos; s {tag}</p>
+                    </Link>
+                  </div>
+                )
+              } else {
+                return null
+              }
+            })}
+            {/* 奇数だからお休み */}
+            {/* <div className={styles.moreSearch}>
+          <Link href="/blog" passHref>
+            <p> 🔍　to Blog List </p>
           </Link>
-        </p>
+        </div> */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Image
+              src="/hero-room.jpg"
+              width={300}
+              height={300}
+              objectFit="contain"
+              alt=""
+            />
+          </div>
+          <NotionBlocks blocks={blocks} />
+          <div className={SecStyles.pcode}>
+            <iframe
+              src="https://bae.herohoro.com/%E3%83%89%E3%83%83%E3%83%88/index.html"
+              width="100%"
+              height="100%"
+              scrolling="no"
+            ></iframe>
+          </div>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogCategoryLink heading="Category List" categorys={categorys} />
+          <BlogTagLink heading="Tag List" tags={tags} />
+          <h3>Prolile</h3>
+          <hr />
+          <div className={styles.flexWraper}>
+            <Image
+              src="/profile.png"
+              width={200}
+              height={200}
+              objectFit="contain"
+              alt=""
+            />
+            <Image
+              src="/notion-essentials-badge.png"
+              width={80}
+              height={80}
+              objectFit="contain"
+              alt=""
+            />
+            <ul>
+              <li>勉強が趣味</li>
+              <li>教えるの好き</li>
+              <li>オンライン学習塾で５画面を操り指導(自称：職人)</li>
+              <li>元教員・介護士</li>
+              <li>家の中では無限大</li>
+            </ul>
+          </div>
+          <h3>Study TimeLine</h3>
+          <hr />
+          <iframe
+            src="https://notion2charts.com/embed/bad01964-6bce-4f62-bc9d-1d2899652ed6"
+            width="100%"
+            height="400"
+          ></iframe>
+
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <TwitterTimeline />
+        </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default RootPage
