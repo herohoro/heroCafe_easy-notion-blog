otoyo/easy-notion-blog

Include GoogleAnalytics component each page.tsx

app/blog/before/[date]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../app/server-constants'
+import GoogleAnalytics from '../../../../components/google-analytics'
 import {
   getRankedPosts,
   getPostsBefore,
@@ -36,36 +37,39 @@ const BlogBeforeDatePage = async ({ params: { date: encodedDate } }) => {
   ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.mainContent}>
-        <header>
-          <h2>Posts before {date.split('T')[0]}</h2>
-        </header>
+    <>
+      <GoogleAnalytics pageTitle={`Posts before ${date.split('T')[0]}`} />
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <header>
+            <h2>Posts before {date.split('T')[0]}</h2>
+          </header>
 
-        <NoContents contents={posts} />
+          <NoContents contents={posts} />
 
-        {posts.map(post => {
-          return (
-            <div className={styles.post} key={post.Slug}>
-              <PostDate post={post} />
-              <PostTags post={post} />
-              <PostTitle post={post} />
-              <PostExcerpt post={post} />
-              <ReadMoreLink post={post} />
-            </div>
-          )
-        })}
+          {posts.map(post => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
 
-        <footer>
-          <NextPageLink firstPost={firstPost} posts={posts} />
-        </footer>
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <BlogTagLink heading="Categories" tags={tags} />
+        </div>
       </div>
-
-      <div className={styles.subContent}>
-        <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        <BlogTagLink heading="Categories" tags={tags} />
-      </div>
-    </div>
+    </>
   )
 }
 

app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../app/server-constants'
+import GoogleAnalytics from '../../components/google-analytics'
 import {
   BlogPostLink,
   BlogTagLink,
@@ -29,32 +30,35 @@ const BlogPage = async () => {
   ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.mainContent}>
-        <NoContents contents={posts} />
+    <>
+      <GoogleAnalytics pageTitle="Blog" />
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <NoContents contents={posts} />
 
-        {posts.map(post => {
-          return (
-            <div className={styles.post} key={post.Slug}>
-              <PostDate post={post} />
-              <PostTags post={post} />
-              <PostTitle post={post} />
-              <PostExcerpt post={post} />
-              <ReadMoreLink post={post} />
-            </div>
-          )
-        })}
+          {posts.map(post => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
 
-        <footer>
-          <NextPageLink firstPost={firstPost} posts={posts} />
-        </footer>
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <BlogTagLink heading="Categories" tags={tags} />
+        </div>
       </div>
-
-      <div className={styles.subContent}>
-        <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        <BlogTagLink heading="Categories" tags={tags} />
-      </div>
-    </div>
+    </>
   )
 }
 

app/blog/tag/[tag]/before/[date]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../../../app/server-constants'
+import GoogleAnalytics from '../../../../../../components/google-analytics'
 import {
   BlogPostLink,
   BlogTagLink,
@@ -39,37 +40,40 @@ const BlogTagBeforeDatePage = async ({ params: { tag: encodedTag, date: encodedD
   ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.mainContent}>
-        <header>
-          <h2>{tag} before {date.split('T')[0]}</h2>
-        </header>
+    <>
+      <GoogleAnalytics pageTitle={`Posts in ${tag} before ${date.split('T')[0]}`} />
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <header>
+            <h2>{tag} before {date.split('T')[0]}</h2>
+          </header>
 
-        <NoContents contents={posts} />
+          <NoContents contents={posts} />
 
-        {posts.map(post => {
-          return (
-            <div className={styles.post} key={post.Slug}>
-              <PostDate post={post} />
-              <PostTags post={post} />
-              <PostTitle post={post} />
-              <PostExcerpt post={post} />
-              <ReadMoreLink post={post} />
-            </div>
-          )
-        })}
+          {posts.map(post => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
 
-        <footer>
-          <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
-        </footer>
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+          <BlogTagLink heading="Categories" tags={tags} />
+        </div>
       </div>
-
-      <div className={styles.subContent}>
-        <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-        <BlogTagLink heading="Categories" tags={tags} />
-      </div>
-    </div>
+    </>
   )
 }
 

app/blog/tag/[tag]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../app/server-constants'
+import GoogleAnalytics from '../../../../components/google-analytics'
 import {
   BlogPostLink,
   BlogTagLink,
@@ -45,35 +46,38 @@ const BlogTagPage = async ({ params: { tag: encodedTag } }) => {
   ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.mainContent}>
-        <header>
-          <h2>{tag}</h2>
-        </header>
+    <>
+      <GoogleAnalytics pageTitle={`Posts in ${tag}`} />
+      <div className={styles.container}>
+        <div className={styles.mainContent}>
+          <header>
+            <h2>{tag}</h2>
+          </header>
 
-        {posts.map(post => {
-          return (
-            <div className={styles.post} key={post.Slug}>
-              <PostDate post={post} />
-              <PostTags post={post} />
-              <PostTitle post={post} />
-              <PostExcerpt post={post} />
-              <ReadMoreLink post={post} />
-            </div>
-          )
-        })}
+          {posts.map(post => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
 
-        <footer>
-          <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
-        </footer>
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+          <BlogTagLink heading="Categories" tags={tags} />
+        </div>
       </div>
-
-      <div className={styles.subContent}>
-        <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-        <BlogTagLink heading="Categories" tags={tags} />
-      </div>
-    </div>
+    </>
   )
 }
 

herohoro/heroCafe_easy-notion-blog

app/blog/before/[date]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../app/server-constants'
+import GoogleAnalytics from '../../../../components/google-analytics'
 import {
   getRankedPosts,
   getPostsBefore,
@@ -44,59 +45,62 @@ const BlogBeforeDatePage = async ({ params: { date: encodedDate } }) => {
   ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.flexWraper}>
-        <div className={styles.mainContent}>
-          <header className={styles.mainTop}>
-            <h2>Posts before {date.split('T')[0]}</h2>
-          </header>
+    <>
+      <GoogleAnalytics pageTitle={`Posts before ${date.split('T')[0]}`} />
+      <div className={styles.container}>
+        <div className={styles.flexWraper}>
+          <div className={styles.mainContent}>
+            <header className={styles.mainTop}>
+              <h2>Posts before {date.split('T')[0]}</h2>
+            </header>
 
-          <div className={styles.mainGallery}>
-            <NoContents contents={posts} />
+            <div className={styles.mainGallery}>
+              <NoContents contents={posts} />
 
-            {posts.map((post) => {
-              return (
-                <div className={styles.post} key={post.Slug}>
-                  <div className={styles.twoColums}>
-                    <PostDate post={post} />
-                    <PostLike post={post} />
+              {posts.map((post) => {
+                return (
+                  <div className={styles.post} key={post.Slug}>
+                    <div className={styles.twoColums}>
+                      <PostDate post={post} />
+                      <PostLike post={post} />
+                    </div>
+                    <PostCategory post={post} />
+                    <PostTitle post={post} />
+                    <PostThumbnail post={post} />
+                    <PostTags post={post} />
+                    <PostExcerpt post={post} />
                   </div>
-                  <PostCategory post={post} />
-                  <PostTitle post={post} />
-                  <PostThumbnail post={post} />
-                  <PostTags post={post} />
-                  <PostExcerpt post={post} />
-                </div>
-              )
-            })}
-          </div>
+                )
+              })}
+            </div>
 
-          <footer>
-            <NextBackPageLink firstPost={firstPost} posts={posts} />
-            <BackPageLink firstPost={firstPost} posts={posts} />
-          </footer>
+            <footer>
+              <NextBackPageLink firstPost={firstPost} posts={posts} />
+              <BackPageLink firstPost={firstPost} posts={posts} />
+            </footer>
+          </div>
+          <div className={styles.subContent}>
+            <RssFeed />
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <BlogTagLink heading="Tag List" tags={tags} />
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+            <TwitterTimeline />
+          </div>
         </div>
-        <div className={styles.subContent}>
-          <RssFeed />
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <BlogTagLink heading="Tag List" tags={tags} />
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-          <TwitterTimeline />
+        <div className={styles.endContent}>
+          <div className={styles.endSection}>
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          </div>
+          <div className={styles.endSection}>
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <TwitterTimeline />
+          </div>
+          <div className={styles.endSection}>
+            <BlogTagLink heading="Tag List" tags={tags} />
+          </div>
         </div>
       </div>
-      <div className={styles.endContent}>
-        <div className={styles.endSection}>
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        </div>
-        <div className={styles.endSection}>
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <TwitterTimeline />
-        </div>
-        <div className={styles.endSection}>
-          <BlogTagLink heading="Tag List" tags={tags} />
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 

app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../app/server-constants'
+import GoogleAnalytics from '../../components/google-analytics'
 import {
   BlogPostLink,
   BlogTagLink,
@@ -37,63 +38,66 @@ const BlogPage = async () => {
   ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.flexWraper}>
-        <div className={styles.mainContent}>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <Image
-              src="/study-hero.jpeg"
-              width={300}
-              height={200}
-              objectFit="contain"
-              alt=""
-            />
-          </div>
-          <NoContents contents={posts} />
-          <div className={styles.mainGallery}>
-            {posts.map((post) => {
-              return (
-                <div className={styles.post} key={post.Slug}>
-                  <div className={styles.twoColums}>
-                    <PostDate post={post} />
-                    <PostLike post={post} />
+    <>
+      <GoogleAnalytics pageTitle="Blog" />
+      <div className={styles.container}>
+        <div className={styles.flexWraper}>
+          <div className={styles.mainContent}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Image
+                src="/study-hero.jpeg"
+                width={300}
+                height={200}
+                objectFit="contain"
+                alt=""
+              />
+            </div>
+            <NoContents contents={posts} />
+            <div className={styles.mainGallery}>
+              {posts.map((post) => {
+                return (
+                  <div className={styles.post} key={post.Slug}>
+                    <div className={styles.twoColums}>
+                      <PostDate post={post} />
+                      <PostLike post={post} />
+                    </div>
+                    <PostCategory post={post} />
+                    <PostTitle post={post} />
+                    <PostThumbnail post={post} />
+                    <PostTags post={post} />
+                    <PostExcerpt post={post} />
                   </div>
-                  <PostCategory post={post} />
-                  <PostTitle post={post} />
-                  <PostThumbnail post={post} />
-                  <PostTags post={post} />
-                  <PostExcerpt post={post} />
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
+            <footer>
+              <NextPageLink firstPost={firstPost} posts={posts} />
+            </footer>
           </div>
-          <footer>
-            <NextPageLink firstPost={firstPost} posts={posts} />
-          </footer>
+
+          <div className={styles.subContent}>
+            <RssFeed />
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <BlogTagLink heading="Tag List" tags={tags} />
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+            <TwitterTimeline />
+          </div>
         </div>
 
-        <div className={styles.subContent}>
-          <RssFeed />
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <BlogTagLink heading="Tag List" tags={tags} />
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-          <TwitterTimeline />
+        <div className={styles.endContent}>
+          <div className={styles.endSection}>
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          </div>
+          <div className={styles.endSection}>
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <TwitterTimeline />
+          </div>
+          <div className={styles.endSection}>
+            <BlogTagLink heading="Tag List" tags={tags} />
+          </div>
         </div>
       </div>
-
-      <div className={styles.endContent}>
-        <div className={styles.endSection}>
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        </div>
-        <div className={styles.endSection}>
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <TwitterTimeline />
-        </div>
-        <div className={styles.endSection}>
-          <BlogTagLink heading="Tag List" tags={tags} />
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 

app/blog/tag/[tag]/before/[date]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../../../app/server-constants'
+import GoogleAnalytics from '../../../../../../components/google-analytics'
 import {
   BlogPostLink,
   BlogTagLink,
@@ -50,60 +51,65 @@ const BlogTagBeforeDatePage = async ({
     ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.flexWraper}>
-        <div className={styles.mainContent}>
-          <header className={styles.mainTop}>
-            <h2>{tag}</h2>
-          </header>
-          <div className={styles.mainGallery}>
-            <NoContents contents={posts} />
+    <>
+      <GoogleAnalytics
+        pageTitle={`Posts in ${tag} before ${date.split('T')[0]}`}
+      />
+      <div className={styles.container}>
+        <div className={styles.flexWraper}>
+          <div className={styles.mainContent}>
+            <header className={styles.mainTop}>
+              <h2>{tag}</h2>
+            </header>
+            <div className={styles.mainGallery}>
+              <NoContents contents={posts} />
 
-            {posts.map((post) => {
-              return (
-                <div className={styles.post} key={post.Slug}>
-                  <div className={styles.twoColums}>
-                    <PostDate post={post} />
-                    <PostLike post={post} />
+              {posts.map((post) => {
+                return (
+                  <div className={styles.post} key={post.Slug}>
+                    <div className={styles.twoColums}>
+                      <PostDate post={post} />
+                      <PostLike post={post} />
+                    </div>
+                    <PostCategory post={post} />
+                    <PostTitle post={post} />
+                    <PostThumbnail post={post} />
+                    <PostTags post={post} />
+                    <PostExcerpt post={post} />
                   </div>
-                  <PostCategory post={post} />
-                  <PostTitle post={post} />
-                  <PostThumbnail post={post} />
-                  <PostTags post={post} />
-                  <PostExcerpt post={post} />
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
+
+            <footer>
+              <NextBackPageLink firstPost={firstPost} posts={posts} tag={tag} />
+              <BackPageLink firstPost={firstPost} posts={posts} />
+            </footer>
           </div>
 
-          <footer>
-            <NextBackPageLink firstPost={firstPost} posts={posts} tag={tag} />
-            <BackPageLink firstPost={firstPost} posts={posts} />
-          </footer>
+          <div className={styles.subContent}>
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <BlogTagLink heading="Tag List" tags={tags} />
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+            <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+            <TwitterTimeline />
+          </div>
         </div>
-
-        <div className={styles.subContent}>
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <BlogTagLink heading="Tag List" tags={tags} />
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-          <TwitterTimeline />
-        </div>
-      </div>
-      <div className={styles.endContent}>
-        <div className={styles.endSection}>
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        </div>
-        <div className={styles.endSection}>
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-        </div>
-        <div className={styles.endSection}>
-          <BlogTagLink heading="Tag List" tags={tags} />
-          <TwitterTimeline />
+        <div className={styles.endContent}>
+          <div className={styles.endSection}>
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          </div>
+          <div className={styles.endSection}>
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+          </div>
+          <div className={styles.endSection}>
+            <BlogTagLink heading="Tag List" tags={tags} />
+            <TwitterTimeline />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 

app/blog/tag/[tag]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../app/server-constants'
+import GoogleAnalytics from '../../../../components/google-analytics'
 import {
   BlogPostLink,
   BlogCategoryLink,
@@ -54,59 +55,62 @@ const BlogTagPage = async ({ params: { tag: encodedTag } }) => {
     ])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.flexWraper}>
-        <div className={styles.mainContent}>
-          <header className={styles.mainTop}>
-            <h2>{tag}</h2>
-          </header>
-          <div className={styles.mainGallery}>
-            <NoContents contents={posts} />
+    <>
+      <GoogleAnalytics pageTitle={`Posts in ${tag}`} />
+      <div className={styles.container}>
+        <div className={styles.flexWraper}>
+          <div className={styles.mainContent}>
+            <header className={styles.mainTop}>
+              <h2>{tag}</h2>
+            </header>
+            <div className={styles.mainGallery}>
+              <NoContents contents={posts} />
 
-            {posts.map((post) => {
-              return (
-                <div className={styles.post} key={post.Slug}>
-                  <div className={styles.twoColums}>
-                    <PostDate post={post} />
-                    <PostLike post={post} />
+              {posts.map((post) => {
+                return (
+                  <div className={styles.post} key={post.Slug}>
+                    <div className={styles.twoColums}>
+                      <PostDate post={post} />
+                      <PostLike post={post} />
+                    </div>
+                    <PostCategory post={post} />
+                    <PostTitle post={post} />
+                    <PostThumbnail post={post} />
+                    <PostTags post={post} />
+                    <PostExcerpt post={post} />
                   </div>
-                  <PostCategory post={post} />
-                  <PostTitle post={post} />
-                  <PostThumbnail post={post} />
-                  <PostTags post={post} />
-                  <PostExcerpt post={post} />
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
+            <footer>
+              <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
+            </footer>
           </div>
-          <footer>
-            <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
-          </footer>
-        </div>
 
-        <div className={styles.subContent}>
-          <RssFeed />
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <BlogTagLink heading="Tag List" tags={tags} />
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-          <TwitterTimeline />
+          <div className={styles.subContent}>
+            <RssFeed />
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <BlogTagLink heading="Tag List" tags={tags} />
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+            <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+            <TwitterTimeline />
+          </div>
+        </div>
+        <div className={styles.endContent}>
+          <div className={styles.endSection}>
+            <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          </div>
+          <div className={styles.endSection}>
+            <BlogCategoryLink heading="Category List" categorys={categorys} />
+            <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+          </div>
+          <div className={styles.endSection}>
+            <BlogTagLink heading="Tag List" tags={tags} />
+            <TwitterTimeline />
+          </div>
         </div>
       </div>
-      <div className={styles.endContent}>
-        <div className={styles.endSection}>
-          <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        </div>
-        <div className={styles.endSection}>
-          <BlogCategoryLink heading="Category List" categorys={categorys} />
-          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-        </div>
-        <div className={styles.endSection}>
-          <BlogTagLink heading="Tag List" tags={tags} />
-          <TwitterTimeline />
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 